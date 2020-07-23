53fd1c4a95ada822e68b29f0469a66dd5bc44b47
moweihao <moweihao2@163.com>
2020-07-23 16:35:07 +0800

    add a cage

--- main.py
+++ main.py
@@ -16,17 +16,23 @@ plane_d = [0, 930]
 plane_rect = plane_rect.move(plane_d)
 #-------------------------------------------
 while True:
-    clock.tick(60)
+    clock.tick(120)
     temp = plane_d #改变前坐标
     pressed = pygame.key.get_pressed() #获得所有键盘按钮的状态
     #print(pressed[pygame.K_RIGHT])  #DEBUG
     if pygame.key.get_focused():  #判断按键操作
-        if pressed[pygame.K_RIGHT]:
+        if pressed[pygame.K_RIGHT] and plane_d[0]+4<=448:
             plane_d[0] +=4
             plane_rect = plane_rect.move(4,0)
-        elif pressed[pygame.K_LEFT]:
+        if pressed[pygame.K_LEFT] and plane_d[0]-4>=0:
             plane_d[0] -=4
             plane_rect = plane_rect.move(-4,0)
+        if pressed[pygame.K_UP] and plane_d[1]-4>=0:
+            plane_d[1] -=4
+            plane_rect = plane_rect.move(0,-4)
+        if pressed[pygame.K_DOWN] and plane_d[1]+4<=930:
+            plane_d[1] +=4
+            plane_rect = plane_rect.move(0,4)
         print(plane_d)
     #检查关闭点击
     for event in pygame.event.get():
